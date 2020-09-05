b9abe6e614efd5b9b406d850fdfc6159b8154506
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-09-05 22:19:25 +0200

    better comments

--- rss-mode.el
+++ rss-mode.el
@@ -1,14 +1,47 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; rss-mode.el --- Support for offlinerss RSS files
+
+;; Copyright (C) 2020 Emad Elsaid
+
+;; Author: Emad Elsaid <emad.elsaid.hamed@gmail.com>
+;; Created: 5 September 2020
+;; Keywords: News
+;; Homepage: https://github.com/emad-elsaid/emacs-rss-mode
+
+;; This file is not part of GNU Emacs.
+
+;; MIT License
+
+;; Copyright (c) 2020 Emad Elsaid
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;; Code:
+
 ;; Variables
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq rss-archive-dir "~/rss/archive/")
 (setq rss-trash-dir "~/rss/trash/")
 (setq rss-dir "~/rss/INBOX")
 (setq rss-entry-buffer-name "*RSS Entry*")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Modes and keymaps
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq rss-directory-mode-map (make-sparse-keymap))
 (define-key rss-directory-mode-map (kbd "RET") 'rss-open-entry)
 (define-key rss-directory-mode-map (kbd "r") 'rss-archive-current-entry)
@@ -45,9 +78,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.rss\\'" . rss-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; XML complementary functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun xml-node-list-children (node)
   (seq-filter 'listp (xml-node-children node)))
 
@@ -59,9 +92,9 @@
 (defun xml-nodes-content (node)
   (apply 'concat (xml-node-children node)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Main functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun rss-entry-feed-filename (file)
   (concat default-directory "../.meta/" (car (split-string file "-")) ".rss"))
 
@@ -111,9 +144,9 @@ RSS entry on your desk, otherwise Archiving it is the way to go"
   (write-region "" nil (concat rss-trash-dir file))
   (delete-file (concat default-directory file)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Interactive functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun rss-archive-current-entry ()
   "Moved the RSS entry to the rss-archive-dir"
   (interactive)
@@ -138,3 +171,4 @@ RSS entry on your desk, otherwise Archiving it is the way to go"
   (interactive)
   (find-file rss-dir)
   (rss-directory-mode))
+(provide 'rss-mode)
